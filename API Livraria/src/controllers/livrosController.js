9503869a620c9c706cc578db58c6592647678e64
lipefreire <felipef12123@gmail.com>
2022-07-03 17:09:53 -0300

    concluído

--- API Livraria/src/controllers/livrosController.js
+++ API Livraria/src/controllers/livrosController.js
@@ -58,6 +58,14 @@ class LivroController {
             }
         })
     }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros)
+        })
+    }
 }
 
 export default LivroController
