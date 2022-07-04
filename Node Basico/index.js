4436510d819de47e501baeedfba54b40f3fa158a
lipefreire <felipef12123@gmail.com>
2022-07-04 17:17:17 -0300

    Em

--- Node Basico/index.js
+++ Node Basico/index.js
@@ -1,11 +1,37 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.green(texto));
-    })
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Arquivo não encontrado neste diretório.'));
 }
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+    }
+    catch(erro) {
+        trataErro(erro);
+    }
+}
+ 
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => chalk.green(console.log(texto)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
 
 pegaArquivo('./arquivos/texto1.md');
