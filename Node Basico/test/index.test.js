c06368d5fd1f2d60bdb8e8119609cf78c568633a
lipefreire <felipef12123@gmail.com>
2022-07-04 20:09:14 -0300

    Concluído

--- Node Basico/test/index.test.js
+++ Node Basico/test/index.test.js
@@ -1,17 +1,27 @@
 const pegaArquivo = require('../index');
 
 const arrayResult = [
-    {
-        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
-    }
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
 ]
 
 describe('pegaArquivo::', () => {
-    it('Deve ser uma função', () => {
-        expect(typeof pegaArquivo).toBe('function');
-    })
-    it('Deve retornar array com resultados', async () => {
-        const resultado = await pegaArquivo('C:/Users/Felipe Lindo/felipe-node/Node Basico/test/arquivos/texto1.md')
-        expect(resultado).toEqual(arrayResult)
-    })
-});
+  it('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function');
+  })
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('C:/Users/Felipe Lindo/felipe-node/Node Basico/test/arquivos/texto1.md') 
+    expect(resultado).toEqual(arrayResult)
+  })
+  it('deve retornar mensagem "não há links"', async () => {
+    const resultado = await pegaArquivo('C:/Users/Felipe Lindo/felipe-node/Node Basico/test/arquivos/texto1_semlinks.md')
+    expect(resultado).toBe('não há links');
+  })
+  it('deve lançar um erro na falta de arquivo', () => {
+    async function capturaErro() {
+      await pegaArquivo('/C:/Users/Felipe Lindo/felipe-node/Node Basico/test/arquivos/')
+      expect(capturaErro).toThrowError(/não há arquivo no caminho/)
+    }
+  })
+})
