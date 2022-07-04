ecf6c77dba0320f2877b04c2471c77fa45ffb95f
lipefreire <felipef12123@gmail.com>
2022-07-04 19:19:19 -0300

    Em andamento...

--- Node Basico/cli.js
+++ Node Basico/cli.js
@@ -1,17 +1,16 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
-const validaURLs = require('./http-validacao')
+const validaURLs = require('./http-validacao');
 
 const caminho = process.argv;
 
-async function processaTexto(caminhoDoArquivo) {
-    const resultado = await pegaArquivo(caminhoDoArquivo[2]);
-    if (caminho[3] === 'validar') {
-        console.log(chalk.yellow('Links validados'), validaURLs(resultado));
-    }
-    else {
-        console.log(chalk.yellow('Lista de links'), resultado);
-    }
+async function processaTexto(caminhoDeArquivo) {
+  const resultado = await pegaArquivo(caminhoDeArquivo[2]);
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
+  } else {
+    console.log(chalk.yellow('lista de links'), resultado);
+  }
 }
 
 processaTexto(caminho);
