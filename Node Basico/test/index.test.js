86fdcd7c098fee781c5ae34dabbf698e6db5a4f1
lipefreire <felipef12123@gmail.com>
2022-07-04 19:49:21 -0300

    Em andamento...

--- Node Basico/test/index.test.js
+++ Node Basico/test/index.test.js
@@ -1,5 +1,17 @@
 const pegaArquivo = require('../index');
 
-test('Deve ser uma função', () => {
-    expect(typeof pegaArquivo).toBe('function');
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('pegaArquivo::', () => {
+    it('Deve ser uma função', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('Deve retornar array com resultados', async () => {
+        const resultado = await pegaArquivo('C:/Users/Felipe Lindo/felipe-node/Node Basico/test/arquivos/texto1.md')
+        expect(resultado).toEqual(arrayResult)
+    })
 });
