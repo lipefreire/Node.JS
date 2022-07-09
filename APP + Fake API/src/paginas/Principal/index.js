f9b5f7f4686ac325336a9e633713819df4efbccc
lipefreire <felipef12123@gmail.com>
2022-07-08 22:44:59 -0300

    Em andamento...

---
+++ APP + Fake API/src/paginas/Principal/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from 'react-native';
+import estilos from './estilos';
+import api from '../../servicos/api';
+import { buscaUsuario } from '../../servicos/api';
+
+export default function Principal({ navigation }) {
+    const [nomeUsuario, setNomeUsuario] = useState('');
+    const [usuario, setUsuario] = useState({});
+
+    async function busca(){
+        const resultado = await buscaUsuario(nomeUsuario)
+
+        setNomeUsuario('')
+        if (resultado) {
+            setUsuario(resultado)
+        }
+        else {
+            Alert.alert('Usuário não encontrado.')
+            setUsuario({})
+        }
+    }
+
+    return (
+        <ScrollView>
+            <View style={estilos.container}>
+                {
+                    usuario?.login && 
+                <>
+                    <View style={estilos.fundo} />
+                    <View style={estilos.imagemArea}>
+                        <Image source={{ uri: usuario.avatar_url }} style={estilos.imagem} />
+                    </View>
+                    <Text style={estilos.textoNome}>{usuario.name}</Text>
+                    <Text style={estilos.textoEmail}>{usuario.email}</Text>
+                    <View style={estilos.seguidoresArea}>
+                        <View style={estilos.seguidores}>
+                            <Text style={estilos.seguidoresNumero}>{usuario.followers}</Text>
+                            <Text style={estilos.seguidoresTexto}>Seguidores</Text>
+                        </View>
+                        <View style={estilos.seguidores}>
+                            <Text style={estilos.seguidoresNumero}>{usuario.following}</Text>
+                            <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity onPress={() => navigation.navigate('Repositorios')}>
+                        <Text style={estilos.repositorios}>
+                            Ver os repositórios
+                        </Text>
+                    </TouchableOpacity>
+                </>
+                }
+
+                <TextInput
+                    placeholder="Busque por um usuário"
+                    autoCapitalize="none"
+                    style={estilos.entrada}
+                    value={nomeUsuario}
+                    onChangeText={setNomeUsuario}}
+                />
+
+                <TouchableOpacity style={estilos.botao}
+                    onPress={busca}
+                >
+                    <Text style={estilos.textoBotao}>
+                        Buscar
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
+}
